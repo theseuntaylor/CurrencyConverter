41d23e47396b77df56a81d2f1a640dd890acac10
Solomon George-Taylor <54375729+theseuntaylor@users.noreply.github.com>
2021-01-12 21:04:40 +0100

    Added the functionality for the values of the views to swap when the user pressed the swap button. Added the functionality for the values of the textview to change based on the selected value of the spinner.
    Signed-off-by: Solomon George-Taylor <54375729+theseuntaylor@users.noreply.github.com>

--- app/src/main/java/com/theseuntaylor/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/theseuntaylor/currencyconverter/MainActivity.kt
@@ -1,27 +1,105 @@
 package com.theseuntaylor.currencyconverter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var toSpinner: Spinner
+    private lateinit var toTextView: TextView
+
+    private lateinit var fromSpinner: Spinner
+    private lateinit var fromTextView: TextView
+
+    private lateinit var swapButton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toSpinner : Spinner = findViewById(R.id.toSpinner)
-        val fromSpinner : Spinner = findViewById(R.id.fromSpinner)
+        findViews()
+        setUpSpinners()
+        setViews()
+        swapViews(swapButton, fromSpinner, toSpinner)
+    }
+
+    private fun findViews() {
+        toSpinner = findViewById(R.id.toSpinner)
+        toTextView = findViewById(R.id.currency_to_textView)
+        fromSpinner = findViewById(R.id.fromSpinner)
+        fromTextView = findViewById(R.id.currency_from_textView)
+        swapButton = findViewById(R.id.swap_spinners_button)
+    }
+
+    private fun setUpSpinners() {
 
         ArrayAdapter.createFromResource(
             this,
             R.array.from_currencies,
             android.R.layout.simple_dropdown_item_1line
-        ).also {
-            adapter -> adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
-            toSpinner.adapter = adapter
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
             fromSpinner.adapter = adapter
         }
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.to_currencies,
+            android.R.layout.simple_dropdown_item_1line
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
+            toSpinner.adapter = adapter
+        }
     }
+
+    private fun setViews() {
+        toSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                toTextView.text = ""
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val text = parent?.getItemAtPosition(position).toString()
+                toTextView.text = text
+            }
+        }
+
+        fromSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                fromTextView.text = ""
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val text = parent?.getItemAtPosition(position).toString()
+                fromTextView.text = text
+            }
+        }
+    }
+
+    private fun swapViews(swapButtons: ImageButton, fromSpinner: Spinner, toSpinner: Spinner) {
+
+        swapButtons.setOnClickListener {
+            val item1: Int = fromSpinner.selectedItemPosition
+            val item2: Int = toSpinner.selectedItemPosition
+
+            toSpinner.setSelection(item1)
+
+            fromSpinner.setSelection(item2)
+
+        }
+    }
+
 }
